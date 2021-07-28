b17de458227ee062ac94f701bcc8c5914ce6ec02
Emily Crook <Emily.A.Crook96@gmail.com>
2021-07-28 18:49:22 -0500

    score is tracked, saved to local storage, and displayed on the leader board

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,8 @@ var mainDisplayEl = document.querySelector(".main-page");
 var startQuiz = document.querySelector("#start-quiz");
 var highScoreBtn = document.querySelector("#score-page");
 var highScores = document.querySelector("#high-scores");
+var endGame = document.querySelector("#end-game");
+var leaderBoard = document.querySelector("#leader-board");
 
 // list of scores
 var questions = [{
@@ -34,7 +36,7 @@ var questions = [{
 ]
 
 // to keep track of score
-// var trackScore = 0;
+var trackScore = 0;
 var quesIndex = 0;
 
 function takeToQuiz() {
@@ -47,7 +49,7 @@ function takeToQuiz() {
 function takeToScores() {
     highScoreBtn.addEventListener("click", function(v) {
         console.log('high score btn clicked', v.target.innerHTML)
-        // highScoreLog();
+        displayLeaderBoard();
     })
 }
 
@@ -63,7 +65,7 @@ function welcomePage(e) {
 function displayQuestions() {
     mainDisplayEl.innerHTML = '';
     questionDisplay.innerHTML= '';
-    highScores.innerHTML = '';
+    leaderBoard.innerHTML = '';
 
     var askQuestion = document.createElement("p");
     askQuestion.textContent = questions[quesIndex].question;
@@ -86,7 +88,7 @@ function displayQuestions() {
             if(questions[quesIndex].answer === e.target.innerHTML) {
                 console.log('answered correctly')
 
-                // trackScore++
+                trackScore++
                 quesIndex++
             } else {
                 console.log('oops, that is wrong')
@@ -96,7 +98,7 @@ function displayQuestions() {
 
             if (quesIndex >= questions.length) {
                 questionDisplay.innerHTML = '';
-                // endGame();
+                displayEndGame();
                 console.log('time to run the endGame')
             } else {
                 displayQuestions()
@@ -105,4 +107,73 @@ function displayQuestions() {
     }
 }
 
+function displayEndGame() {
+    questionDisplay.innerHTML = "";
+    mainDisplayEl.innerHTML = "";
+
+    var congratsScore = document.createElement("h3");
+    congratsScore.textContent = 'Congratulations, you have finished the quiz! Your final score is: ' + trackScore + '!';
+    endGame.appendChild(congratsScore);
+
+    var enterInitials = document.createElement("p");
+    enterInitials.textContent = 'Enter your initials and see where you are on the leader board.';
+    endGame.appendChild(enterInitials);
+
+    var inputInitials = document.createElement("input")
+    inputInitials.setAttribute("type", "text");
+    inputInitials.setAttribute("minlength", "2");
+    inputInitials.setAttribute("maxlength", "3");
+    endGame.appendChild(inputInitials);
+
+    var submitButtonEl = document.createElement("button");
+    submitButtonEl.setAttribute("type", "submit");
+    submitButtonEl.setAttribute("value", "Submit");
+    submitButtonEl.textContent = "Submit";
+    endGame.appendChild(submitButtonEl);
+
+    submitButtonEl.addEventListener("click", function() {
+        var nameArray = []
+        var scoreArray = []
+
+
+        if(localStorage.getItem('name')){
+            nameArray = JSON.parse(localStorage.getItem('name'))
+        }
+
+        if(localStorage.getItem('score')){
+            scoreArray = JSON.parse(localStorage.getItem('score'))
+        }
+
+        
+       nameArray.push(inputInitials.value)
+       scoreArray.push(trackScore)
+
+        localStorage.setItem("name", JSON.stringify(nameArray));
+        localStorage.setItem("score", JSON.stringify(scoreArray));
+        endGame.innerHTML = "";
+
+        displayLeaderBoard();
+    })
+
+}
+
+function displayLeaderBoard() {
+    var leaderIntroEl = document.createElement("h2");
+    leaderIntroEl.textContent = "Here are the top three leaders!"
+    leaderBoard.appendChild(leaderIntroEl);
+
+    var nameArray = JSON.parse(localStorage.getItem('name'))
+    var scoreArray = JSON.parse(localStorage.getItem('score'))
+
+    scoreArray.forEach(function(score, i) {
+        console.log('I in the for each!!!', i)
+        var listScoreEl = document.createElement("li");
+        listScoreEl.textContent = nameArray[i] + " has a score of " + score;
+
+        leaderBoard.appendChild(listScoreEl)
+    })
+}
+
 welcomePage();
+
+// <p>Enter your initials and see where you are on the leader board.</p>
